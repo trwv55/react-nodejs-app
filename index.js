df4cf44db236342a360a71ef4da30d6e3c43b64f
Vitalii Travin <vitalii.trvn@gmail.com>
2022-11-28 01:39:22 +0600

    nodejs react step-by-step app

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post("/auth/register", registerValidation, UserController.register);
 
 app.get("/auth/me", checkAuth, UserController.getMe);
 
-app.get("/posts/me", checkAuth, UserController.getMe);
+app.post("/posts", PostController.create);
 
 app.listen(5000, (err) => {
   if (err) {
